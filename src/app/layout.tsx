5658277917296bec49c027713d62291d2be3c990
Ademola Shasanya <adeshas@gmail.com>
2025-09-09 07:10:05 +0000

    Can we try "Cairo Play" Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
 import PageProgress from '@/components/page-progress';
-import { Encode_Sans } from 'next/font/google';
+import { Cairo_Play } from 'next/font/google';
 import { cn } from '@/lib/utils';
 
-const encodeSans = Encode_Sans({
+const cairoPlay = Cairo_Play({
   subsets: ['latin'],
-  variable: '--font-encode-sans',
+  variable: '--font-cairo-play',
   display: 'swap',
 });
 
@@ -74,7 +74,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("!scroll-smooth", encodeSans.variable)} suppressHydrationWarning>
+    <html lang="en" className={cn("!scroll-smooth", cairoPlay.variable)} suppressHydrationWarning>
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"

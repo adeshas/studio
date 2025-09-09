1ee93acdc12f9528ea897592e349c52ae5cff5d0
Ademola Shasanya <adeshas@gmail.com>
2025-09-09 07:12:18 +0000

    nothing is working. its all using Inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['var(--font-cairo-play)', 'sans-serif'],
+        headline: ['var(--font-cairo-play)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {

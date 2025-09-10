14edf2c5a43f9659f12155117d91f2ce654be620
Ademola Shasanya <adeshas@gmail.com>
2025-09-10 13:52:03 +0000

    use this for the background image https://rmh.jsl.mybluehost.me/wp-conte

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -43,7 +43,7 @@ export default function Expertise() {
     >
         <div className="absolute inset-0 z-0">
             <Image
-                src="https://picsum.photos/seed/lawyer/1920/1080"
+                src="https://rmh.jsl.mybluehost.me/wp-content/uploads/2025/10/IMG_1163_v1.JPEG"
                 alt="Professional legal services"
                 fill
                 className="object-cover"

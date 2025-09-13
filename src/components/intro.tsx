11271327c946819d0243a6a3c5061a7a1f2e1376
Ademola Shasanya <adeshas@gmail.com>
2025-09-13 12:47:49 +0000

    Do it like this

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,12 +4,13 @@ export default function Intro() {
     <section id="intro" className="w-full py-12 md:py-24 lg:py-32 bg-background">
       <div className="container mx-auto px-4 md:px-6">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-light font-headline tracking-tighter sm:text-5xl">
+          <h2 className="text-3xl font-light font-headline tracking-tighter sm:text-4xl">
             Welcome to
           </h2>
           <p className="text-4xl md:text-6xl font-semibold font-headline text-foreground mt-2">
             Oyewole &amp; Adesina
           </p>
+          <div className="mt-6 mx-auto w-24 h-1 bg-primary"></div>
         </div>
       </div>
     </section>

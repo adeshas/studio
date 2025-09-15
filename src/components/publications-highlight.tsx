a73e427dbf6d37fd1ab865da1bf989a72e39e5fd
Ademola Shasanya <adeshas@gmail.com>
2025-09-15 05:20:28 +0000

    change image to this https://rmh.jsl.mybluehost.me/wp-content/uploads/20

--- src/components/publications-highlight.tsx
+++ src/components/publications-highlight.tsx
@@ -62,7 +62,7 @@ export default function PublicationsHighlight() {
             <motion.div key={index} variants={itemVariants}>
                 <Card className="flex flex-col h-full bg-transparent border-0 shadow-none rounded-none">
                     <div className="relative w-full h-64 mb-6">
-                        <Image src="/folder-document-1.png" alt={item.title} fill className="object-cover" />
+                        <Image src="https://rmh.jsl.mybluehost.me/wp-content/uploads/2025/10/folder-document-1.png" alt={item.title} fill className="object-cover" />
                     </div>
                     <div className="flex flex-col flex-grow">
                         <h3 className="text-xl font-bold font-headline mb-2">{item.title}</h3>

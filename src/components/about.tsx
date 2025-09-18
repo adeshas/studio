a717c65a88b57d4ce594f694003667a4c2b1521c
Ademola Shasanya <adeshas@gmail.com>
2025-09-18 17:03:10 +0000

    About Us section: Oyewole & Adesina is a law firm in Lagos Nigeria, with

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -44,7 +44,7 @@ export default function About() {
           <motion.div className="space-y-4" variants={itemVariants}>
             <h2 id="about-us-heading" className="text-3xl font-light font-headline tracking-tighter sm:text-5xl">About Us</h2>
             <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              <span className="font-semibold text-foreground">Oyewole &amp; Adesina</span> is a law firm in Nigeria with proficiency in dispute resolution, corporate and commercial law, real estate, finance, energy, labour and employment, shipping, admiralty and intellectual property law. <span className="font-semibold text-foreground">Oyewole &amp; Adesina</span> is strategically placed to offer quality and first-class cost-efficient legal services to clients across the major economic hubs and sectors of Nigeria.
+              <span className="font-semibold text-foreground">Oyewole &amp; Adesina</span> is a law firm in Lagos Nigeria, with proficiency in dispute resolution, corporate and commercial law, real estate, finance, energy, labour and employment, shipping, admiralty and intellectual property law. <span className="font-semibold text-foreground">Oyewole &amp; Adesina</span> is strategically placed to offer quality and first-class cost-efficient legal services to clients across the major economic hubs and sectors of Nigeria.
             </p>
             <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               We have built a reputation for understanding our clients’ peculiar business issues and challenges, and then proffering a legal and business-savvy solution.

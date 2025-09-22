1dfaf45236d395e2661588615c9f4d84e2cb8018
Ademola Shasanya <adeshas@gmail.com>
2025-09-22 11:00:53 +0000

    Gallery in the menu should be capitalized not uppercase

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ const navLinks = [
   { href: "/our-people", label: "Our People" },
   { href: "/careers", label: "Careers" },
   { href: "/publications", label: "Publications" },
-  { href: "#", label: "GALLERY" },
+  { href: "#", label: "Gallery" },
   { href: "/contact", label: "Contact Us" },
 ];
 

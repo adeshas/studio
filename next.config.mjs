38e0ef9328768a626fe83f1802ea18f6b9832c27
Ademola Shasanya <adeshas@gmail.com>
2025-09-27 19:12:05 +0000

    ready to export try 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+        return config;
+    },
 };
 
 export default nextConfig;

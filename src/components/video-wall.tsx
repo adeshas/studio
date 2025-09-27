78db82bf3f2e0b56f67fc815dc8389df10df9d80
Ademola Shasanya <adeshas@gmail.com>
2025-09-27 17:17:24 +0000

    also remove the roatation of the video wall

--- src/components/video-wall.tsx
+++ src/components/video-wall.tsx
@@ -55,8 +55,8 @@ const VideoWall = () => {
             <p className="max-w-[900px] mx-auto mt-4 text-muted-foreground md:text-xl/relaxed">A dynamic view into the industries we serve and the sectors we help shape.</p>
         </div>
         <div className="w-full h-[40vh] md:h-[50vh] overflow-hidden flex items-center justify-center">
-            <div className="relative w-[150vw] h-[150vh] flex items-center justify-center">
-                <div className="grid grid-cols-1 gap-4 -rotate-[22.5deg] scale-110">
+            <div className="relative w-[150vw] h-auto flex items-center justify-center">
+                <div className="grid grid-cols-1 gap-4 scale-110">
                     <Swiper
                         {...swiperParams}
                         speed={10000}

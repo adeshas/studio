1a39944404127d557ad98f4ddaaa1dabb291ecc4
Ademola Shasanya <adeshas@gmail.com>
2025-09-27 19:14:57 +0000

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    webpack: (config) => {
-        config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-        return config;
-    },
 };
 
 export default nextConfig;
